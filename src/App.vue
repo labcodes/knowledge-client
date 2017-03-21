429a45b6a20811c7dd5185b10afa910de0c60c2c
Thulio Philipe <thulioph@gmail.com>
2017-03-21 16:22:07 -0300

    Adds the header component and use bulma as a ui framework.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
+    <kn-header></kn-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import knHeader from './components/Header';
+
 export default {
   name: 'app',
+
+  components: {
+    knHeader,
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  body {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  @import "../node_modules/bulma/bulma.sass";
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,76 @@
+<template>
+  <nav class="nav">
+    <div class="nav-left">
+      <a class="nav-item">
+        Knowledge
+      </a>
+    </div>
+
+    <div class="nav-center">
+      <a class="nav-item" :href="repoUrl" target="_blank">
+        <span class="icon">
+          <i class="fa fa-github"></i>
+        </span>
+      </a>
+    </div>
+
+    <span
+      class="nav-toggle"
+      @click="toggleNavbar()"
+      :class="{ 'is-active': isActive }">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+
+    <div
+      class="nav-right nav-menu"
+      :class="{ 'is-active': isActive }">
+
+      <router-link
+        class="nav-item"
+        title="Home"
+        to="/"
+        exact>
+        Home
+      </router-link>
+
+      <router-link
+        class="nav-item"
+        title="Login"
+        to="/login"
+        exact>
+        Login
+      </router-link>
+
+      <router-link
+        class="nav-item"
+        title="Links"
+        to="/links"
+        exact>
+        Links
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'header',
+
+  data() {
+    return {
+      isActive: false,
+      repoUrl: 'https://github.com/labcodes/knowledge-client',
+    };
+  },
+
+  methods: {
+    toggleNavbar() {
+      this.isActive = !this.isActive;
+    },
+  },
+};
+</script>
+
+<style></style>
