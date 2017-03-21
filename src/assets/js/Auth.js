8a9f1ed15fe82de4a683085db06a35b669138ecd
Thulio Philipe <thulioph@gmail.com>
2017-03-21 18:51:40 -0300

    Import the localstorage in auth service.

--- src/assets/js/Auth.js
+++ src/assets/js/Auth.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import LocalStorage from './Localstorage';
 
 class Auth {
   constructor(data) {
     this.formData = data;
     this.apiUrl = 'http://localhost:3000/api';
     this.http = Vue.http;
+
+    this.storage = new LocalStorage('userInfo');
   }
 
   login() {
@@ -15,6 +18,7 @@ class Auth {
     this.http.post(this.url, { username, password })
       .then((response) => {
         console.warn(response.body);
+        this.storage.set(response.body);
       })
       .catch((err) => {
         console.error(err);
