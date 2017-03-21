b176e3262a665236fb97a8c774f652fd704d1bf6
Thulio Philipe <thulioph@gmail.com>
2017-03-21 15:12:25 -0300

    Adds the login and link router and initial component structure.

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+
+// Components
+import Hello from '../components/Hello';
+import Login from '../components/Login';
+import Links from '../components/Links';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history', // removes the # from URL
+  saveScrollPosition: true,
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+    {
+      path: '/links',
+      name: 'Links',
+      component: Links,
     },
   ],
 });
