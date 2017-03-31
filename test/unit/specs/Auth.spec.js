7d32574f12591958de701d8077b104184f24af2c
Thulio Philipe <thulioph@gmail.com>
2017-03-31 18:53:51 -0300

    Adds the initial test to Auth file.

--- test/unit/specs/Auth.spec.js
+++ test/unit/specs/Auth.spec.js
@@ -1,4 +1,3 @@
-import FetchMock from 'fetch-mock';
 import Auth from '@/assets/js/Auth';
 
 describe('Auth.', () => {
@@ -10,7 +9,6 @@ describe('Auth.', () => {
 
   afterEach(() => {
     sandbox.restore();
-    FetchMock.restore();
   });
 
   it('should set the data correctly.', () => {
@@ -23,19 +21,5 @@ describe('Auth.', () => {
 
     expect(auth.data.username).to.be.equal('user');
     expect(auth.data.password).to.be.equal('123');
-  });
-
-  it('should make the login correctly.', () => {
-    const credentials = {
-      username: 'user',
-      password: '123',
-    };
-
-    FetchMock.mock('/accounts/login/', credentials);
-
-    const auth = new Auth(credentials);
-    const login = auth.login();
-
-    console.warn(login);
   });
 });
