d0b2e1e65bfece6d7605a153135776256187e3c8
Fernando Lins <fernando@labcodes.com.br>
2017-05-10 19:56:21 -0300

    Add token authorization to get links from backend

--- src/assets/js/ApiService.js
+++ src/assets/js/ApiService.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import Event from './Event';
+import Localstorage from './Localstorage';
+
 
 import AppConfig from '../config';
 
@@ -7,10 +9,14 @@ class ApiService {
   constructor() {
     this.apiUrl = AppConfig.apiUrl;
     this.event = Event;
+    this.storage = new Localstorage('userInfo');
+    const authToken = this.storage.get().auth_token;
+    axios.defaults.headers.common.Authorization = `Token ${authToken}`;
   }
 
   getLinks() {
     this.url = `${this.apiUrl}/links/`;
+
 
     axios
       .get(this.url)
