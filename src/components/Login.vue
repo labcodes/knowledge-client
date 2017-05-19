f5da5f34546bdaa06fc0fc8f610cf701f2ed7cc6
thulioph <thulioph@gmail.com>
2017-05-19 13:53:35 -0300

    Adjusts the login HTML markup and grid.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,51 +1,76 @@
 <template>
-  <section class="is-half is-offset-one-quarter">
-    <div class="column">
-      <form @submit.prevent="handleLogin">
-        <fieldset class="field">
-          <label for="email" class="label">E-mail</label>
-          <div class="control">
-            <input
-              type="email"
-              class="input"
-              placeholder="email@provider.com"
-              id="email"
-              required
-              v-model="form.username">
-          </div>
-        </fieldset>
+  <div>
+    <kn-subheader
+      title="Login"
+      subtitle="Faça o login para ter acesso ao nosso conteúdo.">
+    </kn-subheader>
 
-        <fieldset class="field">
-          <label for="password" class="label">Password</label>
-          <div class="control">
-            <input
-              type="password"
-              class="input"
-              placeholder="******"
-              id="password"
-              required
-              pattern=".{6,}"
-              title="6 characters minimum"
-              v-model="form.password">
-          </div>
-        </fieldset>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <form @submit.prevent="handleLogin">
+              <fieldset class="field">
+                <label for="email" class="label">
+                  E-mail
+                </label>
 
-        <button
-          type="submit"
-          class="button is-primary"
-          :class="{ 'is-loading': form.isLoading }">
-          Login
-        </button>
-      </form>
-    </div>
-  </section>
+                <div class="control">
+                  <input
+                    type="email"
+                    class="input"
+                    placeholder="email@provider.com"
+                    id="email"
+                    required
+                    v-model="form.username">
+                </div>
+              </fieldset>
+
+              <fieldset class="field">
+                <label for="password" class="label">
+                  Password
+                </label>
+
+                <div class="control">
+                  <input
+                    type="password"
+                    class="input"
+                    placeholder="******"
+                    id="password"
+                    required
+                    pattern=".{6,}"
+                    title="6 characters minimum"
+                    v-model="form.password">
+                </div>
+              </fieldset>
+
+              <button
+                type="submit"
+                class="button is-primary"
+                :class="{ 'is-loading': form.isLoading }">
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+// Components
+import knSubheader from '../components/Subheader';
+
+// Assets
 import Event from '../assets/js/Event';
 
 export default {
   name: 'login',
+
+  components: {
+    knSubheader,
+  },
 
   data() {
     return {
@@ -76,7 +101,6 @@ export default {
       Object.assign(this.$data, this.$options.data());
     },
   },
-
 };
 </script>
 
